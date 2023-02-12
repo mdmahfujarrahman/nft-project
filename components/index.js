431697745fe757912989a6b6666c40eb9003158f
mdmahfujarrahman <ahmedmahfuj28@gmail.com>
2023-02-12 13:34:30 +0600

    top seller added

--- components/index.js
+++ components/index.js
@@ -1,5 +1,7 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Button from "./Button";
+import Banner from "./Banner";
+import CreatorCard from "./CreatorCard";
 
-export { Footer, Navbar, Button };
+export { Footer, Navbar, Button, Banner, CreatorCard };
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,121 @@
-import React from "react";
+import { useEffect, useRef, useState } from "react";
+
+import Image from "next/image";
+import { useTheme } from "next-themes";
+import { Banner, CreatorCard } from "../components";
+import images from "../assets";
+import { makeID } from "../utils/makeId";
 
 const Home = () => {
-    return <div className="text-3xl text-red-500">Home</div>;
+    const parentRef = useRef(null);
+    const [hideButton, setHideButton] = useState(false);
+    const childRef = useRef(null);
+    const { theme } = useTheme();
+
+    const handleScroll = (direction) => {
+        const { current } = childRef;
+        const scroolAmount = window.innerWidth > 1800 ? 270 : 310;
+        if (direction === "left") {
+            current.scrollLeft -= scroolAmount;
+        }
+        if (direction === "right") {
+            current.scrollLeft += scroolAmount;
+        }
+    };
+
+    const isScrollable = () => {
+        const { current } = childRef;
+        const { current: parent } = parentRef;
+
+        if (current?.scrollWidth >= parent?.offsetWidth) {
+            setHideButton(false);
+        } else {
+            setHideButton(true);
+        }
+    };
+
+    useEffect(() => {
+        isScrollable();
+        window.addEventListener("resize", isScrollable);
+        return () => {
+            window.removeEventListener("resize", isScrollable);
+        };
+    }, []);
+
+    return (
+        <div className="flex justify-center sm:px-4 p-12">
+            <div className="w-full minlg:w-4/5">
+                <Banner
+                    title="Discover, collect, and sell extraordinary NFTs"
+                    parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+                    childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
+                />
+                <div>
+                    <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xm:ml-0">
+                        Top Sellers
+                    </h1>
+                    <div
+                        ref={parentRef}
+                        className="relative flex-1 max-w-full flex mt-3"
+                    >
+                        <div
+                            ref={childRef}
+                            className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none"
+                        >
+                            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, i) => {
+                                return (
+                                    <CreatorCard
+                                        rank={item}
+                                        creatorImage={images[`creator${item}`]}
+                                        item={item}
+                                        creatorName={`0x${makeID(3)}...${makeID(
+                                            4
+                                        )}`}
+                                        key={i}
+                                        creatorEths={10 - item * 0.5}
+                                    />
+                                );
+                            })}
+                            {!hideButton && (
+                                <>
+                                    <div
+                                        onClick={() => handleScroll("left")}
+                                        className="absolute w-8 h-8 minlg:w-12 minlg:h-12 left-0 top-45 cursor-pointer"
+                                    >
+                                        <Image
+                                            layout="fill"
+                                            objectFit="contain"
+                                            className={
+                                                theme === "light" &&
+                                                "filter invert"
+                                            }
+                                            alt="left aroow"
+                                            src={images.left}
+                                        />
+                                    </div>
+                                    <div
+                                        onClick={() => handleScroll("right")}
+                                        className="absolute w-8 h-8 minlg:w-12 minlg:h-12 right-0 top-45 cursor-pointer"
+                                    >
+                                        <Image
+                                            layout="fill"
+                                            objectFit="contain"
+                                            className={
+                                                theme === "light" &&
+                                                "filter invert"
+                                            }
+                                            alt="right aroow"
+                                            src={images.right}
+                                        />
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
